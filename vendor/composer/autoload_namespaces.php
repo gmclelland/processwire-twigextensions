881edfab1fc216d6fe6d5f7b4a45303a1acb149f
Glenn McLelland <glenn.mclelland@gmail.com>
2018-04-26 10:23:38 -0500

    Ran composer update to update dependencies

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -8,4 +8,5 @@ $baseDir = dirname($vendorDir);
 return array(
     'Twig_Extensions_' => array($vendorDir . '/twig/extensions/lib'),
     'Twig_' => array($vendorDir . '/twig/twig/lib'),
+    'PW\\Composer\\' => array($vendorDir . '/hari/pw-module/src'),
 );
